4522101e7ad8c0ba7ce75eabe730158c5b37d82f
Spencer Gunning <swcgunning@gmail.com>
2024-04-27 11:14:02 -0400

    Add cover letters, Umami integration, and Lighthouse script

---
+++ scripts/lighthouse.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Calculate timestamp and make a directory with it
+timestamp=$(date +%y%m%d)
+mkdir reports/$timestamp
+mkdir reports/$timestamp/mobile
+mkdir reports/$timestamp/desktop
+
+# Generate report for each .pug file
+for file in ./src/*; do
+    if ! [[ -d $file ]]; then
+        file=$(basename -- "$file")
+        fname="${file%.*}"
+        if [[ $fname == "index" ]]; then
+           echo "Generating reports for index.html..."
+           lighthouse "https://cering.dev" --quiet=true --chrome-flags="--headless" --output=html --preset=desktop --output-path=reports/$timestamp/desktop/$fname.html
+           lighthouse "https://cering.dev" --quiet=true --chrome-flags="--headless" --output=html --output-path=reports/$timestamp/mobile/$fname.html
+        elif [[ $fname != "$" ]] && [[ $fname != "robots" ]]; then
+           echo "Generating reports for ${fname}.html..."
+           lighthouse "https://cering.dev/${fname}" --quiet=true --chrome-flags="--headless" --output=html --preset=desktop --output-path=reports/$timestamp/desktop/$fname.html
+           lighthouse "https://cering.dev/${fname}" --quiet=true --chrome-flags="--headless" --output=html --output-path=reports/$timestamp/mobile/$fname.html
+        fi
+    fi
+done
