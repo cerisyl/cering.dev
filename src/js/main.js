e1cd0a42c3301963d1bed71aaf3bf664429b4e15
Spencer Gunning <swcgunning@gmail.com>
2024-08-29 11:45:05 -0400

    Resume update - 29 August 2024

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,5 @@
 import * as nav from './nav';
 import resume_default from 'url:../includes/resume/default.pug';
-import resume_lowiro from 'url:../includes/resume/lowiro.pug';
 import resume_dev from 'url:../includes/resume/dev.pug';
 import resume_it from 'url:../includes/resume/it.pug';
 import resume_graphics from 'url:../includes/resume/graphics.pug';
@@ -33,37 +32,30 @@ if (page.includes('resume') || page.includes('$')) {
             const hash = window.location.hash.slice(1);
             const target = document.getElementById('resume');
             const links = document.getElementById('resume-links');
-            if (hash === 'lowiro') {
-                req(resume_lowiro).then(html => target.innerHTML = html);
-                links.innerHTML = `
-                <a class="action" href="./docs/SpencerGunning-Resume_20240323-lowiro.pdf">download as pdf</a>
-                <a class="action" href="./games">view portfolio</a>
-                <a class="action" href="./resume">view general resume</a>
-                `
-            } else if (hash === 'dev') {
+            if (hash === 'dev') {
                 req(resume_dev).then(html => target.innerHTML = html);
                 links.innerHTML = `
-                <a class="action" href="./docs/SpencerGunning-Resume_20240617-dev.pdf">download as pdf</a>
+                <a class="action" href="./docs/SpencerGunning-Resume_20240829-dev.pdf">download as pdf</a>
                 <a class="action" href="./webdev">view portfolio</a>
                 <a class="action" href="./resume">view general resume</a>
                 `
             } else if (hash === 'graphics') {
                 req(resume_graphics).then(html => target.innerHTML = html);
                 links.innerHTML = `
-                <a class="action" href="./docs/SpencerGunning-Resume_20240617-graphics.pdf">download as pdf</a>
+                <a class="action" href="./docs/SpencerGunning-Resume_20240829-graphics.pdf">download as pdf</a>
                 <a class="action" href="./graphics">view portfolio</a>
                 <a class="action" href="./resume">view general resume</a>
                 `
             } else if (hash === 'it') {
                 req(resume_it).then(html => target.innerHTML = html);
                 links.innerHTML = `
-                <a class="action" href="./docs/SpencerGunning-Resume_20240617-it.pdf">download as pdf</a>
+                <a class="action" href="./docs/SpencerGunning-Resume_20240829-it.pdf">download as pdf</a>
                 <a class="action" href="./resume">view general resume</a>
                 `
             } else {
                 req(resume_default).then(html => target.innerHTML = html);
                 links.innerHTML = `
-                <a class="action" href="./docs/SpencerGunning-Resume_20240617-default.pdf">download as pdf</a>
+                <a class="action" href="./docs/SpencerGunning-Resume_20240829.pdf">download as pdf</a>
                 `
             }
         });
